66c5ec62815b03f6801d8cf0f5bf069a76981eb7
Nicholas <nwilliams770@gmail.com>
2017-11-04 22:58:28 -0700

    Callback and Closure Practice

--- W5D4/IntroJSProbs.js
+++ W5D4/IntroJSProbs.js
@@ -67,3 +67,52 @@ function printNSkip3(n) {
 }
 
 printNSkip3(15);
+
+function titleize(names, callback) {
+  let titleized = names.map(name => `Mx. ${name} Jingleheimer Schmidt`);
+  callback(titleized);
+}
+
+function Elephant(name, height, tricks) {
+  this.name = name;
+  this.height = height;
+  this.tricks = tricks;
+}
+
+Elephant.prototype.trumpet = function () {
+  console.log(`${this.name} goes the elephant goes 'phrRRRRRRRRRRR!!!!!!!'`);
+};
+
+Elephant.prototype.grow = function () {
+  this.height = this.height+ 12;
+};
+
+Elephant.prototype.addTrick = function (trick) {
+  this.tricks.push(trick);
+};
+
+Elephant.prototype.play = function () {
+  let index = Math.floor(Math.random() * this.tricks.length);
+  console.log(`${this.name} is ${this.tricks[index]}!`);
+};
+
+let ellie = new Elephant("Ellie", 185, ["giving human friends a ride", "playing hide and seek"]);
+let charlie = new Elephant("Charlie", 200, ["painting pictures", "spraying water for a slip and slide"]);
+let kate = new Elephant("Kate", 234, ["writing letters", "stealing peanuts"]);
+let micah = new Elephant("Micah", 143, ["trotting", "playing tic tac toe", "doing elephant ballet"]);
+
+let herd = [ellie, charlie, kate, micah];
+
+Elephant.paradeHelper = function (elephant) {
+  console.log(`${elephant.name} is skeptical about JavaScript!`);
+};
+
+function dinerBreakfast() {
+  let order = "I'd like cheesy scrambled eggs please.";
+  console.log(order);
+
+  return function (food) {
+    order = `${order.slice(0, order.length)} and ${food}, NOW!`;
+    console.log(order);
+  };
+}
