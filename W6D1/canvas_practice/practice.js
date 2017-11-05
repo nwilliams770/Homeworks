1ee0e4a6e69234eb81f92de570a64fac04ea5696
Nicholas <nwilliams770@gmail.com>
2017-11-05 00:29:59 -0700

    First Commit

---
+++ W6D1/canvas_practice/practice.js
@@ -0,0 +1,125 @@
+document.addEventListener("DOMContentLoaded", function(){
+  const doodlez = document.getElementById("mycanvas");
+  doodlez.width = 500;
+  doodlez.height = 500;
+
+  const ctx = doodlez.getContext("2d");
+  ctx.fillStyle = "rgb(231, 209, 255)";
+  ctx.fillRect(0, 0, 500, 500);
+
+
+  // let circleShop = function () {
+  //   ctx.beginPath ();
+  //   ctx.arc(375, 150, 50, 0, 160);
+  //   ctx.fillStyle = "rgb(237, 255, 245)";
+  //   ctx.fill();
+  // };
+  let hat =
+    ctx.rect(200, 0, 50, 50);
+    ctx.fillStyle = "rgb(237, 255, 245)";
+    ctx.fill();
+
+    ctx.rect(250, 0, 50, 50);
+    ctx.fillStyle = "rgb(237, 255, 245)";
+    ctx.fill();
+
+    ctx.rect(150, 0, 50, 50);
+    ctx.fillStyle = "rgb(237, 255, 245)";
+    ctx.fill();
+
+
+
+
+
+    ctx.rect(300, 50, 50, 50);
+    ctx.fillStyle = "rgb(237, 255, 245)";
+    ctx.fill();
+
+    ctx.rect(250, 50, 50, 50);
+    ctx.fillStyle = "rgb(237, 255, 245)";
+    ctx.fill();
+
+    ctx.rect(200, 50, 50, 50);
+    ctx.fillStyle = "rgb(237, 255, 245)";
+    ctx.fill();
+
+    ctx.rect(150, 50, 50, 50);
+    ctx.fillStyle = "rgb(237, 255, 245)";
+    ctx.fill();
+
+    ctx.rect(100, 50, 50, 50);
+    ctx.fillStyle = "rgb(237, 255, 245)";
+    ctx.fill();
+
+
+
+
+
+    ctx.rect(350, 50, 50, 100);
+    ctx.fillStyle = "rgb(237, 255, 245)";
+    ctx.fill();
+
+    ctx.rect(300, 50, 50, 100);
+    ctx.fillStyle = "rgb(237, 255, 245)";
+    ctx.fill();
+
+    ctx.rect(250, 50, 50, 100);
+    ctx.fillStyle = "rgb(237, 255, 245)";
+    ctx.fill();
+
+    ctx.rect(200, 50, 50, 100);
+    ctx.fillStyle = "rgb(237, 255, 245)";
+    ctx.fill();
+
+    ctx.rect(150, 50, 50, 100);
+    ctx.fillStyle = "rgb(237, 255, 245)";
+    ctx.fill();
+
+    ctx.rect(100, 50, 50, 100);
+    ctx.fillStyle = "rgb(237, 255, 245)";
+    ctx.fill();
+
+    ctx.rect(50, 50, 50, 100);
+    ctx.fillStyle = "rgb(237, 255, 245)";
+    ctx.fill();
+
+
+
+
+    ctx.rect(350, 50, 50, 150);
+    ctx.fillStyle = "rgb(237, 255, 245)";
+    ctx.fill();
+
+    ctx.rect(250, 50, 50, 150);
+    ctx.fillStyle = "rgb(237, 255, 245)";
+    ctx.fill();
+
+    ctx.rect(200, 50, 50, 150);
+    ctx.fillStyle = "rgb(237, 255, 245)";
+    ctx.fill();
+
+    ctx.rect(150, 50, 50, 150);
+    ctx.fillStyle = "rgb(237, 255, 245)";
+    ctx.fill();
+
+    ctx.rect(50, 50, 50, 150);
+    ctx.fillStyle = "rgb(237, 255, 245)";
+    ctx.fill();
+
+
+});
+
+
+
+// const circle = new Path2D();
+//   circle.moveTo (150, 400);
+//   circle.arc(100, 35, 25, 0, 2 * Math.PI);
+//   ctx.fillStyle = "rgb(246, 255, 244)";
+
+
+
+//     const ctx = canvasPractice.getContext("2d");
+//     ctx.fillStyle = "red";
+//     ctx.fillRect = (0, 0, 500, 500);
+//
+// });
